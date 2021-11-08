8a06eb00a8ea0fe1d825f4fbc2a8447948b75178
Cnly <minecnly@gmail.com>
2021-11-08 22:59:02 +0800

    Add referrer policy and font hacks

--- config/mediawiki/BaseCustomSettings.php
+++ config/mediawiki/BaseCustomSettings.php
@@ -72,3 +72,42 @@ $wgNamespaceContentModels[NS_USER_TALK] = 'flow-board';
 wfLoadExtension( 'Linter' );  # Requires database changes
 wfLoadExtension( 'DiscussionTools' );  # Requires database changes
 $wgFragmentMode = [ 'html5' ];
+
+wfLoadExtension( 'HeadScript' );
+$headScriptReferrerPolicy = <<< 'START_END_MARKER'
+<meta name="referrer" content="same-origin">
+START_END_MARKER;
+$headScriptExternalFonts = <<< 'START_END_MARKER'
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+START_END_MARKER;
+$headScriptFontStyles = <<< 'START_END_MARKER'
+<style>
+@font-face {
+	font-family: Hiragino;
+	font-weight: 400;
+	src: local(Hiragino Sans W3), local(Hiragino Sans CNS), local(Hiragino Sans GB);
+}
+@font-face {
+	font-family: Hiragino;
+	font-weight: 700;
+	src: local(Hiragino Sans W6), local(Hiragino Sans CNS), local(Hiragino Sans GB);
+}
+body {
+	--font-family-base: Inter, Hiragino, sans-serif;
+}
+body.ios-fonts {
+	--font-family-base: Inter, 'PingFang TC', 'PingFang SC', sans-serif;
+}
+</style>
+<script>
+// Since iOS lacks fonts like Hiragino CNS, GB, etc.
+document.addEventListener("DOMContentLoaded", function(event) {
+	let isIOS = /iPad|iPhone|iPod/.test(navigator.platform) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+	if (isIOS)
+		document.body.className += ' ios-fonts';
+});
+</script>
+START_END_MARKER;
+$wgHeadScriptCode = $headScriptReferrerPolicy . $headScriptExternalFonts . $headScriptFontStyles;
